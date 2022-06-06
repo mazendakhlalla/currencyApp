505e38495a783b1c0b6e971270ba39421b1612b9
mazendakhlalla <mazen.dakhlalla@gmail.com>
2022-06-06 09:54:09 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  dToE: number;
+  eToD: number;
+  usdToEur(u: string) {
+    this.dToE = parseFloat((parseFloat(u) * 0.92).toFixed(2));
+    return u;
+  }
+  eurToUsd(e: string) {
+    this.eToD = parseFloat((parseFloat(e) * 1.09).toFixed(2));
+    return e;
+  }
   title = 'currencyApp';
 }
